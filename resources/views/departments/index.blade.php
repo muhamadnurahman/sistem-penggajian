c1f4db4d62064b084398a95ee1034a5ca83a6ece
muhamadnurahman <muhamadnurahman2006@gmail.com>
2026-03-27 13:29:29 +0700

    add : template dashboard

---
+++ resources/views/departments/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <header class="mb-3">
+        <a href="#" class="burger-btn d-block d-xl-none">
+            <i class="bi bi-justify fs-3"></i>
+        </a>
+    </header>
+    <div class="page-heading">
+        <h3>Departments</h3>
+    </div>
+
+    <section class="section">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Department List</h4>
+            </div>
+@endsection
