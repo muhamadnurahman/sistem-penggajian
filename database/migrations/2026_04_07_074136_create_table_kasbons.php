842f01858a137f1ce813e2ff48316577c07d6c8d
muhamadnurahman <muhamadnurahman2006@gmail.com>
2026-04-08 13:31:37 +0700

    migration kasbon

---
+++ database/migrations/2026_04_07_074136_create_table_kasbons.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kasbons', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('employee_id')->constrained('employees');
+            $table->decimal('amount', 10, 2);
+            $table->string('description')->nullable();
+            $table->enum('status', ['pending','approved','rejected'])->default('pending');
+            $table->boolean('is_paid')->default(false);
+            $table->date('date')->default(now());
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kasbons');
+    }
+};
